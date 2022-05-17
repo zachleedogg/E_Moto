baa3189f481bee4752133ba8d75944b4451600ca
zachleedogg <zach.levenberg@gmail.com>
2022-05-16 21:57:50 -0700

    added DBF tool

--- Firmware/CAN/dbc_json_to_DBF.py
+++ Firmware/CAN/dbc_json_to_DBF.py
@@ -5,15 +5,17 @@ file = 'C:\\Repos\\E_Moto\\Firmware\\CAN\\dbc.json' #easygui.fileopenbox("select
 
 dbc_dict = json.load(open(file))
 
-print("//******************************BUSMASTER Messages and signals Database ******************************//\n")
-print("[DATABASE_VERSION] 1.3\n")
-print("[PROTOCOL] CAN\n")
-print("[BUSMASTER_VERSION] [3.2.2]\n")
+f_DBF = open('C:\\Repos\\E_Moto\\Firmware\\CAN\\emoto.dbf', 'w')
+
+f_DBF.write("//******************************BUSMASTER Messages and signals Database ******************************//\n\n")
+f_DBF.write("[DATABASE_VERSION] 1.3\n\n")
+f_DBF.write("[PROTOCOL] CAN\n\n")
+f_DBF.write("[BUSMASTER_VERSION] [3.2.2]\n\n")
 num_mess = 0
 for node in dbc_dict["NODE"]:
     for message in node['messages']:
         num_mess += 1
-print("[NUMBER_OF_MESSAGES] {}\n".format(num_mess))
+f_DBF.write("[NUMBER_OF_MESSAGES] {}\n\n".format(num_mess))
 
 base_address = 0x1
 
@@ -24,9 +26,13 @@ for node in dbc_dict["NODE"]:
         if id is None:
             id = base_address
             base_address +=1
-        print("[START_MSG] {}_{},{},{},{},{},{}".format(node['name'], message['name'], id, "8", len(message['signals']),1, "X" if message.get("x_id") else "S"))
+        f_DBF.write("[START_MSG] {}_{},{},{},{},{},{}\n".format(node['name'], message['name'], id, "8", len(message['signals']), 1, "X" if message.get("x_id") else "S"))
         offset = 0
         for signal in message['signals']:
-            print("[START_SIGNALS] {},{},{},{},{},{},{},{},{},{},{},{}".format(signal['name'],signal['length'],str(int(offset/8) + 1),str(offset%8),"U",2**int(signal['length'])-1,0,1,0.000000,1.00000,"",""))
+            if signal["length"] == 1:
+                data_type = "B"
+            else:
+                data_type = "U"
+            f_DBF.write("[START_SIGNALS] {},{},{},{},{},{},{},{},{},{},{},{}\n".format(signal['name'], signal['length'], int(offset/8) + 1, float(offset%8), data_type, 2**signal['length']-1, 0, 1, signal["offset"], signal["scale"],"",""))
             offset += int(signal['length'])
-        print("[END_MSG]\n")
+        f_DBF.write("[END_MSG]\n\n")
